64c80bace51ceaaea4f5d57f0781556ceada954b
faroooq <faroooq.in@gmail.com>
2022-07-10 12:44:32 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,12 +11,14 @@ export const environment = {
     measurementId: "G-T2T63SJVWS"
   },
   default_imageUrl: "https://www.stackmi.com/assets/images/stackmi_seo.png",
-  midtierurl: "http://localhost:4000",
+  // LOCAL 4000
+  // midtierurl: "http://localhost:4000",
+  // PROD
+  midtierurl: "https://api.stackmi.com",
   articleurl: "https://www.stackmi.com/academy/article/",
   eventurl: "https://www.stackmi.com/events/event/",
   defaulturl: "https://www.stackmi.com/",
   my_imageUrl: "https://stackmi-storage.s3.ap-south-1.amazonaws.com/add_seo_image.png",
-  // midtierurl: "https://api.weeazy.org/api",
   gClientId: "114364532011-qu5le8pp4nd6k18e3vjscn18423djjrk.apps.googleusercontent.com",
   gClientSecret: "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",
   fAppId: '2189034417964830',
